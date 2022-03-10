5a9e094a2620707ff63ba694768d5a966929edf8
dormiry <aamack9@gmail.com>
2022-03-09 19:04:50 -0800

    initial commit

---
+++ script/carousel.js
@@ -0,0 +1,114 @@
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
+const dotsNav = document.querySelector('.carousel__nav');
+const dots = Array.from(dotsNav.children);
+const channelSwitchSound = new Audio('assets/channel-switch.mp3');
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+const tvWidth = document.querySelector('.tv').width;
+
+
+// Arrange the slides next to one another
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index * 4 + 'px';
+};
+
+slides.forEach(setSlidePosition);
+
+
+// FUNCTIONS
+    const moveToSlide = (track, currentSlide, targetSlide) => {
+        track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+        currentSlide.classList.remove('current-slide');
+        targetSlide.classList.add('current-slide');
+    }
+
+    const updateDots = (currentDot, targetDot) => {
+        currentDot.classList.remove('current-slide');
+        targetDot.classList.add('current-slide');
+    }
+
+    const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+        if (targetIndex === 0) {
+            prevButton.classList.add('is-hidden');
+            nextButton.classList.remove('is-hidden');
+        } else if (targetIndex === slides.length - 1) {
+            prevButton.classList.remove('is-hidden');
+            nextButton.classList.add('is-hidden');
+        } else {
+            prevButton.classList.remove('is-hidden');
+            nextButton.classList.remove('is-hidden');
+        }
+    }
+
+    function channelSwitchImg(index) {
+        if (index === 0) {
+            document.querySelector('.channel-switch').src="assets/channel-switch1.png";
+        } else if (index === 1) {
+            document.querySelector('.channel-switch').src="assets/channel-switch2.png";
+        } else if (index === 2) {
+            document.querySelector('.channel-switch').src="assets/channel-switch3.png";
+        } else if (index === 3) {
+            document.querySelector('.channel-switch').src="assets/channel-switch4.png";
+        } else if (index === 4) {
+            document.querySelector('.channel-switch').src="assets/channel-switch5.png";
+        } else {
+            document.querySelector('.channel-switch').src="assets/channel-switch6.png";
+        }
+    }    
+
+
+// When I click left, moves slides to the left
+prevButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const prevDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+    moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
+    channelSwitchImg(prevIndex);
+    channelSwitchSound.play();
+})
+
+// When I click right, move slides to the right
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
+  
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
+    channelSwitchImg(nextIndex);
+    channelSwitchSound.play();
+    
+
+})
+
+// When I click the nav indicators, move to that slide
+dotsNav.addEventListener('click', e => {
+    // Find what indicator is clicked on
+    const targetDot = e.target.closest('button');
+
+    if(!targetDot) return;
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
+    channelSwitchImg(targetIndex);
+    channelSwitchSound.play();
+
+})
---
+++ script/main.js
@@ -0,0 +1,77 @@
+/* POWER SWITCH */
+    const powerSwitch = document.querySelector('.power-switch');
+    const turnOnAnimation = document.querySelector('.tv__start-animation');
+    const turnOffAnimation = document.querySelector('.tv__off-animation');
+    let clicks = 0;
+    const powerSwitchSound = new Audio('assets/power-switch-click.mp3');
+    const arrow = document.querySelector('.arrow');
+    const indicators = document.querySelector('.carousel__nav');
+    const arrowRemoveR = document.querySelector('.carousel__button--right');
+    const arrowRemoveL = document.querySelector('.carousel__button--left');
+
+    // Check if number of clicks on the Power Switch are Odd (Turn on) or Even (Turn off)
+    function powerSwitchClick(clicks) {
+        if (clicks%2 == 0) {
+            console.log('The number is even');
+            turnOffAnimation.play();
+            arrowRemoveR.classList.add('hidden-power-off');
+            arrowRemoveL.classList.add('hidden-power-off');
+            myMedia.pause();
+            turnOffAnimation.addEventListener('ended',turnOff,false);
+                function turnOff(e) {
+                    turnOnAnimation.style.display = "block";
+                    turnOnAnimation.currentTime = 0;
+                    turnOffAnimation.style.display = "none";
+                }
+        } else {
+            console.log('The number is odd');
+            turnOnAnimation.play();
+            arrowRemoveR.classList.remove('hidden-power-off');
+            arrowRemoveL.classList.remove('hidden-power-off');
+            playBGMusic(.30);
+            turnOnAnimation.addEventListener('ended',turnOn,false);
+                function turnOn(e) {
+                    turnOnAnimation.style.display = "none";
+                    turnOffAnimation.style.display = "block";
+                    turnOffAnimation.currentTime = 0;
+                }
+            }
+    }
+
+    // Event for Power Switch click
+    powerSwitch.addEventListener('click', e => {
+        clicks += 1;
+        powerSwitchSound.play();
+        powerSwitchClick(clicks);
+    })
+
+
+/* VOLUME SLIDER (BG-MUSIC) (Jquery) */
+
+$("#volume").slider({
+    min: 0,
+    max: 100,
+    value: 30,
+    range: "min",
+    orientation: 'vertical',
+    slide: function(event, ui) {
+    setVolume(ui.value / 100);
+    }
+  });
+  
+  // Create HTML <audio class="myMedia">
+  var myMedia = document.createElement('audio');
+  $('#player').append(myMedia);
+  myMedia.id = "myMedia";
+  myMedia.src = 'assets/dream-a-little-dream-of-me.mp3';
+
+  function playBGMusic(myVolume) {
+          myMedia.setAttribute('loop', 'loop');
+          setVolume(myVolume);
+          myMedia.play();
+  }
+  
+  function setVolume(myVolume) {
+    var myMedia = document.getElementById('myMedia');
+    myMedia.volume = myVolume;
+  }
